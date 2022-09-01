e7e61e6714f4d8ff7ec24b0145bd611e276172f9
lydiakatsis <72734966+lydiakatsis@users.noreply.github.com>
2022-09-01 18:12:32 +0100

    Adjusted run_model.py
    If file length is longer than 4 hours or less than 30 files, then does predictions on files one by one, else uses batches.

--- Gunshot classification GUI/.ipynb_checkpoints/run_model-checkpoint.py
+++ Gunshot classification GUI/.ipynb_checkpoints/run_model-checkpoint.py
@@ -6,6 +6,7 @@
 #==========Import Utilities==========
 from opensoundscape.torch.models.cnn import load_outdated_model
 from opensoundscape.torch.models.cnn import load_model
+from opensoundscape.audio import Audio
 import opensoundscape
 
 # Other utilities and packages
@@ -20,17 +21,10 @@ import os
 from datetime import date
 from tqdm.notebook import tqdm_notebook
 import psutil
-import sys
-
 
 start_time = time.time()
 
-
-# As CPU implementation in opso currently isn't working, this step uses num workers = 0 if on windows computer, otherwise counts the number of CPUs and utilises all
-if sys.platform.startswith('win'):
-    cpus = 0
-else:
-    cpus = psutil.cpu_count()
+cpus = psutil.cpu_count()
 
 #==========Load Model==========
 url = 'https://www.dropbox.com/s/qnno1u648fa8ovd/outdated_model.model?dl=0'
@@ -41,45 +35,106 @@ subprocess.run(['curl',
 
 model = load_model('model1.model')
 
+
 #=========Preprocess Audio==========
 file_list = glob('*.WAV') + glob('*.wav')
+file_length = Audio.from_file(file_list[1]).duration()
+no_files = len(file_list)
+
 audio_file_df = pd.DataFrame(index=file_list)
-
-
-file_length = len(file_list)
-
 full_scores = pd.DataFrame()
 full_preds = pd.DataFrame()
 
-for i in range(file_length):
-    #time.sleep(0.05)
-    t1 = int(time.time())
-    print("Classifying file  " + str(i+1) + " of " + str(file_length) +'...')    
+if file_length > 14400:
+    for i in range(file_length):
+        #time.sleep(0.05)
+        t1 = int(time.time())
+        print("Classifying file  " + str(i+1) + " of " + str(file_length) +'...')    
+        
+        df = audio_file_df[i:i +1]
     
-    df = audio_file_df[i:i +1]
-
-#==========Generate Predictions==========
-    scores, preds, unsafe = model.predict(
-                    df,
-                    batch_size=64,
-                    num_workers=cpus,
-                    activation_layer="softmax",
-                    binary_preds='single_target'
-                )
-
-
-    t2 = int(time.time())
-    print("Finished classifying file " +  str(i+1) + ' in ' + str(int(t2 - t1)) + ' seconds!\n')
+    #==========Generate Predictions==========
+        scores, preds, unsafe = model.predict(
+                        df,
+                        batch_size=64,
+                        num_workers=cpus,
+                        activation_layer="softmax",
+                        binary_preds='single_target'
+                    )
     
-    full_scores = pd.concat([full_scores,scores])
-    full_scores['folder_name'] = os.path.basename(os.getcwd())
+    
+        t2 = int(time.time())
+        print("Finished classifying file " +  str(i+1) + ' in ' + str(int(t2 - t1)) + ' seconds!\n')
+        
+        full_scores = pd.concat([full_scores,scores])
+        full_scores['folder_name'] = os.path.basename(os.getcwd())
+    
+    full_scores.sort_values(by=['positive'], ascending=False, inplace=True)
+    full_scores.to_csv("GUNSHOT_scores.csv")
+    
+    end_time = time.time()
+
+elif int(no_files) < 30:
+    for i in range(int(file_length)):
+        #time.sleep(0.05)
+        t1 = int(time.time())
+        print("Classifying file  " + str(i+1) + " of " + str(file_length) +'...')    
+        
+        df = audio_file_df[i:i +1]
+    
+    #==========Generate Predictions==========
+        scores, preds, unsafe = model.predict(
+                        df,
+                        batch_size=64,
+                        num_workers=cpus,
+                        activation_layer="softmax",
+                        binary_preds='single_target'
+                    )
+    
+    
+        t2 = int(time.time())
+        print("Finished classifying file " +  str(i+1) + ' in ' + str(int(t2 - t1)) + ' seconds!\n')
+        
+        full_scores = pd.concat([full_scores,scores])
+        full_scores['folder_name'] = os.path.basename(os.getcwd())
+    
+    full_scores.sort_values(by=['positive'], ascending=False, inplace=True)
+    full_scores.to_csv("GUNSHOT_scores.csv")
+    
+    end_time = time.time()
+    
+else:   
+    batch_size = int((len(file_list)/10)+0.5)    
+    batches = [(i*batch_size,(i+1)*batch_size) if i < 9 else (i*batch_size,len(file_list)) for i in range(10)]
+
+    for i in range(len(batches)):
+        t1 = int(time.time())
+        print("Classifying batch  " + str(i+1) + " of " + str(len(batches)) +'...')   
+        df = audio_file_df[batches[i][0]:batches[i][1]]
+        
+        #==========Generate Predictions==========
+        scores, preds, unsafe = model.predict(
+                        df,
+                        batch_size=64,
+                        num_workers=cpus,
+                        activation_layer="softmax",
+                        binary_preds='single_target'
+                    )
+    
+    
+        t2 = int(time.time())
+        print("Finished classifying batch " +  str(i+1) + ' in ' + str(int(t2 - t1)) + ' seconds!\n')
+        
+        full_scores = pd.concat([full_scores,scores])
+        full_scores['folder_name'] = os.path.basename(os.getcwd())
+
 
 full_scores.sort_values(by=['positive'], ascending=False, inplace=True)
 full_scores.to_csv("GUNSHOT_scores.csv")
 
 end_time = time.time()
 
-
+        
 print("Finished classifying all files in  " + str(int((end_time - start_time)/60)) + ' minutes!\n\n')
 
 print('You should now be able to inspect your results in the csv file saved to this folder called GUNSHOT_scores.csv.\n\nOpen the next program, Results_validation to inspect your results. ' )  
